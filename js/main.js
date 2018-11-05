8d94c2d1e332d1936a1b50fbe2c797fe301ac5ce
Abdelrhmangad <24657115+Abdelrhmangad@users.noreply.github.com>
2018-11-05 22:20:40 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -21,3 +21,26 @@ up.addEventListener('click', function io() {
         behavior: 'smooth'
     });
 });
+
+
+//--------------------------------------
+
+var one = document.getElementById('nomber1'),
+    two = document.getElementById('nomber2'),
+    three = document.getElementById('nomber3'),
+    four = document.getElementById('nomber4'),
+    i;
+
+for (i = 0; i < Math.floor(Math.random() * 1238); i = i + 1) {
+    two.innerHTML = i + 1;
+}
+for (i = 0; i < Math.floor(Math.random() * 1238); i = i + 1) {
+    three.innerHTML = i + 1;
+}
+for (i = 0; i < Math.floor(Math.random() * 1238); i = i + 1) {
+    four.innerHTML = i + 1;
+}
+
+for (i = 0; i < Math.floor(Math.random() * 1238); i = i + 1) {
+    one.innerHTML = i + 1;
+}
